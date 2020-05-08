d6d671ff04218d2474c416710d5d0ed1438e849a
Matthew <mattcorralejo1@gmail.com>
2020-05-08 02:35:23 -0700

    "commiting"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 100}
+    validates :category, presence: true, inclusion: { in: %w(Fiction Non-Fiction)}
+    
+
 end
